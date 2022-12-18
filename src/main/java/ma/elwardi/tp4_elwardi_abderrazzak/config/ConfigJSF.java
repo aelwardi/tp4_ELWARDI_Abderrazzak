32bb0a3f4ef483c9d2e3ad75f949eab88cb73c9f
a.elwardi <a.elwardi98gmail.Com>
2022-12-18 14:58:37 +0100

    Configuration Files "Web.xml" et ConfigJSF

---
+++ src/main/java/ma/elwardi/tp4_elwardi_abderrazzak/config/ConfigJSF.java
@@ -0,0 +1,11 @@
+
+package ma.elwardi.tp4_elwardi_abderrazzak.config;
+
+import jakarta.faces.annotation.FacesConfig;
+import jakarta.faces.bean.ApplicationScoped;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
